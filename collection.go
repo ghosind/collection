622af86ae6604dfac9b493c80ed23ff1ea75252c
Chen Su <ghosind@gmail.com>
2022-07-20 17:56:39 +0800

    feat: add collection interface.

---
+++ collection.go
@@ -0,0 +1,34 @@
+package collection
+
+// Collection is the root interface for this collections framework hierarchy.
+type Collection[T comparable] interface {
+	// Add adds the specified element to this collection.
+	Add(e T) bool
+
+	// AddAll adds all of the elements in the this collection.
+	AddAll(c ...T) bool
+
+	// Clear removes all of the elements from this collection.
+	Clear()
+
+	// Contains returns true if this collection contains the specified element.
+	Contains(e T) bool
+
+	// ContainsAll returns true if this collection contains all of the elements in the specified collection.
+	ContainsAll(c ...T) bool
+
+	// IsEmpty returns true if this collection contains no elements.
+	IsEmpty() bool
+
+	// Remove removes the specified element from this collection.
+	Remove(e T) bool
+
+	// RemoveAll removes all of the elements in the specified collection from this collection.
+	RemoveAll(c ...T) bool
+
+	// Size returns the number of elements in this collection.
+	Size() int
+
+	// ToSlice returns a slice containing all of the elements in this collection.
+	ToSlice() []T
+}
