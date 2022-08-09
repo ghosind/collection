e121de1571bff5197c40b584a7f1513c37f253b7
ghosind <ghosind@gmail.com>
2022-08-09 22:12:10 +0800

    test: test ConcurrentHashSet's Clone and Equals methods.

--- concurrent_hashset_test.go
+++ concurrent_hashset_test.go
@@ -14,6 +14,41 @@ func TestConcurrentHashSet(t *testing.T) {
 	testSet[*testStruct](t, NewConcurrentHashSet[*testStruct](), pointerData)
 }
 
+func TestConcurrentHashSetCloneAndEquals(t *testing.T) {
+	set1 := NewConcurrentHashSet[int]()
+	set1.Add(1)
+	set1.Add(2)
+	set1.Add(3)
+
+	if set1.Equals(1) {
+		t.Errorf("set1.Equals(1) return true, expect false")
+	}
+
+	set2 := NewConcurrentHashSet[string]()
+	if set1.Equals(set2) {
+		t.Errorf("set1.Equals(set2) return true, expect false")
+	}
+
+	set3 := NewConcurrentHashSet[int]()
+	set3.Add(1)
+	if set1.Equals(set3) {
+		t.Errorf("set1.Equals(set3) return true, expect false")
+	}
+
+	set4 := set1.Clone()
+	if !set1.Equals(set4) {
+		t.Errorf("set1.Equals(set4) return false, expect true")
+	}
+
+	set5 := NewConcurrentHashSet[int]()
+	set5.Add(1)
+	set5.Add(2)
+	set5.Add(4)
+	if set1.Equals(set5) {
+		t.Errorf("set1.Equals(set5) return true, expect false")
+	}
+}
+
 func TestConcurrentHashSetForEach(t *testing.T) {
 	set := NewConcurrentHashSet[int]()
 
