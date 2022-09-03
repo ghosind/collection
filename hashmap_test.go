0e49c3a23e6bd83e79d1da44c35b988b0e2c5671
ghosind <ghosind@gmail.com>
2022-09-03 18:50:22 +0800

    test: test HashMap.Clone and Equals.

--- hashmap_test.go
+++ hashmap_test.go
@@ -1,7 +1,43 @@
 package collection
 
-import "testing"
+import (
+	"math/rand"
+	"testing"
+)
 
 func TestHashMap(t *testing.T) {
 	testMap(t, NewHashMap[int, int]())
+}
+
+func TestHashMapCloneAndEquals(t *testing.T) {
+	data := rand.Perm(10)
+	m := NewHashMap[int, int]()
+
+	testMapPut(t, m, data)
+
+	newMap := m.Clone()
+	if !m.Equals(newMap) {
+		t.Errorf("HashMap.Equals(newMap) returns false, expect true")
+	}
+
+	newMap.ForEach(func(k, v int) error {
+		newMap.Put(k, v+1)
+		return nil
+	})
+	if m.Equals(newMap) {
+		t.Errorf("HashMap.Equals(1) returns true, expect false")
+	}
+
+	newMap.Clear()
+	if m.Equals(newMap) {
+		t.Errorf("HashMap.Equals(newMap) returns true, expect false")
+	}
+
+	if m.Equals(NewHashMap[string, int]()) {
+		t.Errorf("HashMap.Equals(NewHashMap[string, int]()) returns true, expect false")
+	}
+
+	if m.Equals(1) {
+		t.Errorf("HashMap.Equals(1) returns true, expect false")
+	}
 }
