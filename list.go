5cb277955f12b77350d7f0fa12804c569e0022dc
ghosind <ghosind@gmail.com>
2023-12-06 23:33:53 +0800

    refactor: refactor list interface.

--- list.go
+++ list.go
@@ -1,9 +1,26 @@
 package collection
 
+// List is an ordered collection.
 type List[T any] interface {
-	Add(T)
+	Collection[T]
 
-	AddAll(...T)
+	// AddAtIndex inserts the specified element to the specified position in this list.
+	AddAtIndex(int, T)
 
-	Insert(int, T)
+	// Get returns the element at the specified position inn this list.
+	Get(int) T
+
+	// IndexOf returns the index of the first occurrence of the specified element in this list, or -1
+	// if this list does not contain the element.
+	IndexOf(T) int
+
+	// LastIndexOf returns the index of the last occurrence of the specified element in this list, or
+	// -1 if this list does not contain the element.
+	LastIndexOf(T) int
+
+	// RemoveAtIndex removes the element at the specified position in this list.
+	RemoveAtIndex(int) T
+
+	// Set replaces the element at the specified position in this list with the specified element.
+	Set(int, T) T
 }
