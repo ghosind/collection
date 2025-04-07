71b73b643f4e644404d9833bf110083be06217ba
Chen Su <ghosind@gmail.com>
2025-04-07 16:02:05 +0800

    test: dict.Iter should not be implemented before go 1.23
    Signed-off-by: Chen Su <ghosind@gmail.com>

--- dict/dict_pre_go123_test.go
+++ dict/dict_pre_go123_test.go
@@ -3,9 +3,15 @@
 package dict
 
 import (
+	"reflect"
+
 	"github.com/ghosind/go-assert"
 )
 
 func testDictIter(a *assert.Assertion, constructor dictTestConstructor) {
-	// Do nothing
+	d := constructor()
+
+	ty := reflect.TypeOf(d)
+	_, ok := ty.MethodByName("Iter")
+	a.NotTrueNow(ok)
 }
