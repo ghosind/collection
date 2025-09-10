afec4d69647d7894b6f088e2cb60c1e4f9fcec00
Chen Su <ghosind@gmail.com>
2025-09-10 13:34:01 +0800

    test: add test cases for list types
    Signed-off-by: Chen Su <ghosind@gmail.com>

---
+++ list/list_pre_go123_test.go
@@ -0,0 +1,19 @@
+//go:build !go1.23
+
+package list
+
+import "github.com/ghosind/go-assert"
+
+func testListIter(a *assert.Assertion, constructor listConstructor) {
+	l := constructor()
+
+	l.AddAll(testData...)
+	a.EqualNow(len(testData), l.Size())
+	res := make([]int, 0, l.Size())
+
+	for v := range l.Iter() {
+		res = append(res, v)
+	}
+
+	a.EqualNow(testData, res)
+}
