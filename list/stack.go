cd426a075404a1e64ee6c065069d2e2288638663
Chen Su <ghosind@gmail.com>
2025-12-31 16:00:49 +0800

    feat(list): update Stack type
    - Add NewStackFrom.
    - Override Stack's Clone, Equals, and String to fix logic error.
    
    Signed-off-by: Chen Su <ghosind@gmail.com>

--- list/stack.go
+++ list/stack.go
@@ -1,5 +1,11 @@
 package list
 
+import (
+	"bytes"
+
+	"github.com/ghosind/collection/internal"
+)
+
 // Stack represents a stack data structure based on ArrayList.
 type Stack[T any] struct {
 	ArrayList[T]
@@ -10,9 +16,33 @@ func NewStack[T any]() *Stack[T] {
 	return &Stack[T]{}
 }
 
+// NewStackFrom creates and returns a new Stack containing the elements of the
+// provided collection.
+func NewStackFrom[T any](c []T) *Stack[T] {
+	stack := NewStack[T]()
+	for _, e := range c {
+		stack.Push(e)
+	}
+	return stack
+}
+
 // topIndex returns the index of the top element in the stack.
 func (s *Stack[T]) topIndex() int {
 	return s.Size() - 1
+}
+
+func (s *Stack[T]) Clone() *Stack[T] {
+	clone := NewStack[T]()
+	clone.ArrayList = *(s.ArrayList.Clone().(*ArrayList[T]))
+	return clone
+}
+
+func (s *Stack[T]) Equals(o any) bool {
+	other, ok := o.(*Stack[T])
+	if !ok {
+		return false
+	}
+	return s.ArrayList.Equals(&other.ArrayList)
 }
 
 // Peek returns the element at the top of this stack without removing it.
@@ -28,4 +58,18 @@ func (s *Stack[T]) Pop() T {
 // Push adds an element to the top of this stack.
 func (s *Stack[T]) Push(e T) {
 	s.Add(e)
+}
+
+// String returns the string representation of this stack.
+func (s *Stack[T]) String() string {
+	buf := bytes.NewBufferString("stack[")
+	for i := 0; i < s.Size(); i++ {
+		if i > 0 {
+			buf.WriteString(" ")
+		}
+		buf.WriteString(internal.ValueString(s.Get(i)))
+	}
+	buf.WriteString("]")
+
+	return buf.String()
 }
--- list/stack_test.go
+++ list/stack_test.go
@@ -22,6 +22,11 @@ func TestStack(t *testing.T) {
 	a.EqualNow(2, stack.Size())
 	a.EqualNow(20, stack.Peek())
 
+	a.EqualNow(stack.String(), "stack[10 20]")
+
+	stack2 := NewStackFrom([]int{10, 20})
+	a.TrueNow(stack2.Equals(stack))
+
 	v := stack.Pop()
 	a.EqualNow(20, v)
 	a.EqualNow(1, stack.Size())
@@ -34,4 +39,40 @@ func TestStack(t *testing.T) {
 	// Pop/Peek on empty should panic with ErrOutOfBounds
 	a.PanicOfNow(func() { stack.Pop() }, collection.ErrOutOfBounds)
 	a.PanicOfNow(func() { stack.Peek() }, collection.ErrOutOfBounds)
+}
+
+func TestStack_Clone(t *testing.T) {
+	a := assert.New(t)
+	stack := NewStack[int]()
+	stack.Push(10)
+	stack.Push(20)
+
+	clone := stack.Clone()
+	a.TrueNow(clone.Equals(stack))
+	a.EqualNow(2, clone.Size())
+	a.EqualNow(20, clone.Peek())
+
+	v := clone.Pop()
+	a.EqualNow(20, v)
+	a.EqualNow(1, clone.Size())
+	a.EqualNow(10, clone.Peek())
+
+	a.NotTrueNow(clone.Equals(stack))
+
+	// Original stack should remain unchanged
+	a.EqualNow(2, stack.Size())
+	a.EqualNow(20, stack.Peek())
+}
+
+func TestStack_Equals(t *testing.T) {
+	a := assert.New(t)
+	stack1 := NewStackFrom([]int{1, 2, 3})
+	stack2 := NewStackFrom([]int{1, 2, 3})
+	stack3 := NewStackFrom([]int{1, 2, 4})
+	stack4 := NewStackFrom([]int{1, 2, 3, 4})
+
+	a.TrueNow(stack1.Equals(stack2))
+	a.NotTrueNow(stack1.Equals(stack3))
+	a.NotTrueNow(stack1.Equals(stack4))
+	a.NotTrueNow(stack1.Equals("not a stack"))
 }
